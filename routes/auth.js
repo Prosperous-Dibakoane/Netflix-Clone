3b32dbce49ed5d9e641c95bc4e0bd19c48125b1b
Prosperous Dibakoane <iamprosperous20@gmail.com>
2025-08-05 18:18:39 +0200

    added an api endpoint to show the login form

--- routes/auth.js
+++ routes/auth.js
@@ -8,3 +8,7 @@ const passport = require('../config/passport');
 router.get('/', (req, res) => {
   res.render('home', { session: req.session });
 });
+
+router.get("/login", (req, res) => {
+  res.render("login");
+});
