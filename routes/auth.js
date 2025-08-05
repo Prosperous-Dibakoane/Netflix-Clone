a2e50e98e8c8aa7235980c53a66884e67cdc8806
Prosperous Dibakoane <iamprosperous20@gmail.com>
2025-08-05 18:16:46 +0200

    added an api endpoint to render the hompage

--- routes/auth.js
+++ routes/auth.js
@@ -4,3 +4,7 @@ const bcrypt = require("bcrypt");
 const { User } = require("../models");
 const db = require('../models');
 const passport = require('../config/passport');
+
+router.get('/', (req, res) => {
+  res.render('home', { session: req.session });
+});
