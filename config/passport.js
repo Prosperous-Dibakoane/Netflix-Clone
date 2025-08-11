6596fa017479db492ddc751000692cff651cf82c
prozzzz <iamprosperous20@gmail.com>
2025-08-11 17:58:03 +0200

    Set Google OAuth callback URL for authentication flow

--- config/passport.js
+++ config/passport.js
@@ -5,3 +5,5 @@ const db = require('../models');
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: "/auth/google/callback"
+},
